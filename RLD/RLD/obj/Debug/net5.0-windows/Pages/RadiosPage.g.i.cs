a74a58d45f755c9072f4ad0f1c3b428127a40ed1
BorysKopytko <kopytkoborys@gmail.com>
2021-11-11 02:33:57 +0200

    Deleted unused files

--- RLD/RLD/obj/Debug/net5.0-windows/Pages/RadiosPage.g.i.cs
+++ RLD/RLD/obj/Debug/net5.0-windows/Pages/RadiosPage.g.i.cs
@@ -53,7 +53,7 @@ namespace RLD.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/RLD;component/pages/radiospage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/RLD;V1.0.0.0;component/pages/radiospage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Pages\RadiosPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
