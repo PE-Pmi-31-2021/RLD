96e4cf0122500da14104be1394bd8de3a4b3df64
BorysKopytko <kopytkoborys@gmail.com>
2021-11-25 02:02:58 +0200

    Created dark and light themes

--- RLD/RLD/Presentation/Pages/Cards.xaml.cs
+++ RLD/RLD/Presentation/Pages/Cards.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,111 @@ namespace RLD.Pages
     /// </summary>
     public partial class Cards : Page
     {
+        BitmapImage booksIcon = new BitmapImage();
+        BitmapImage cardsIcon = new BitmapImage();
+        BitmapImage radiosIcon = new BitmapImage();
+        BitmapImage RLDIcon = new BitmapImage();
+        BitmapImage settingsIcon = new BitmapImage();
+
         public Cards()
         {
             InitializeComponent();
+
+            using (var db = new ApplicationContext())
+            {
+                if (db.Settings.Where(item => item.Name == "Theme").FirstOrDefault().Value == "Dark")
+                {
+                    RLDLabel.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                    radiosLabel.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                    booksLabel.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                    cardsLabel.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                    settingsLabel.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+
+                    RLDButton.Background = new SolidColorBrush(Color.FromRgb(45, 45, 45));
+                    radiosButton.Background = new SolidColorBrush(Color.FromRgb(45, 45, 45));
+                    booksButton.Background = new SolidColorBrush(Color.FromRgb(45, 45, 45));
+                    cardsButton.Background = new SolidColorBrush(Color.FromRgb(45, 45, 45));
+                    settingsButton.Background = new SolidColorBrush(Color.FromRgb(45, 45, 45));
+
+                    RLDButton.Background = new SolidColorBrush(Color.FromRgb(45, 45, 45));
+                    radiosButton.Background = new SolidColorBrush(Color.FromRgb(45, 45, 45));
+                    booksButton.Background = new SolidColorBrush(Color.FromRgb(45, 45, 45));
+                    cardsButton.Background = new SolidColorBrush(Color.FromRgb(45, 45, 45));
+                    settingsButton.Background = new SolidColorBrush(Color.FromRgb(45, 45, 45));
+
+                    booksIcon.BeginInit();
+                    booksIcon.StreamSource = new MemoryStream(Icons.IconsResources.BooksIconDark);
+                    booksIcon.EndInit();
+                    booksIconXAML.Source = booksIcon;
+
+                    cardsIcon.BeginInit();
+                    cardsIcon.StreamSource = new MemoryStream(Icons.IconsResources.CardsIconDark);
+                    cardsIcon.EndInit();
+                    cardsIconXAML.Source = cardsIcon;
+
+                    radiosIcon.BeginInit();
+                    radiosIcon.StreamSource = new MemoryStream(Icons.IconsResources.RadiosIconDark);
+                    radiosIcon.EndInit();
+                    radiosIconXAML.Source = radiosIcon;
+
+                    RLDIcon.BeginInit();
+                    RLDIcon.StreamSource = new MemoryStream(Icons.IconsResources.RLDIconDark);
+                    RLDIcon.EndInit();
+                    RLDIconXAML.Source = RLDIcon;
+
+                    settingsIcon.BeginInit();
+                    settingsIcon.StreamSource = new MemoryStream(Icons.IconsResources.SettingsIconDark);
+                    settingsIcon.EndInit();
+                    settingsIconXAML.Source = settingsIcon;
+
+                    
+
+                }
+
+                else if (db.Settings.Where(item => item.Name == "Theme").FirstOrDefault().Value == "Light")
+                {
+
+
+                    RLDLabel.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                    radiosLabel.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                    booksLabel.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                    cardsLabel.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                    settingsLabel.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+
+                    RLDButton.Background = new SolidColorBrush(Color.FromRgb(235, 235, 235));
+                    radiosButton.Background = new SolidColorBrush(Color.FromRgb(235, 235, 235));
+                    booksButton.Background = new SolidColorBrush(Color.FromRgb(235, 235, 235));
+                    cardsButton.Background = new SolidColorBrush(Color.FromRgb(235, 235, 235));
+                    settingsButton.Background = new SolidColorBrush(Color.FromRgb(235, 235, 235));
+
+                    booksIcon.BeginInit();
+                    booksIcon.StreamSource = new MemoryStream(Icons.IconsResources.BooksIconLight);
+                    booksIcon.EndInit();
+                    booksIconXAML.Source = booksIcon;
+
+                    cardsIcon.BeginInit();
+                    cardsIcon.StreamSource = new MemoryStream(Icons.IconsResources.CardsIconLight);
+                    cardsIcon.EndInit();
+                    cardsIconXAML.Source = cardsIcon;
+
+                    radiosIcon.BeginInit();
+                    radiosIcon.StreamSource = new MemoryStream(Icons.IconsResources.RadiosIconLight);
+                    radiosIcon.EndInit();
+                    radiosIconXAML.Source = radiosIcon;
+
+                    RLDIcon.BeginInit();
+                    RLDIcon.StreamSource = new MemoryStream(Icons.IconsResources.RLDIconLight);
+                    RLDIcon.EndInit();
+                    RLDIconXAML.Source = RLDIcon;
+
+                    settingsIcon.BeginInit();
+                    settingsIcon.StreamSource = new MemoryStream(Icons.IconsResources.SettingsIconLight);
+                    settingsIcon.EndInit();
+                    settingsIconXAML.Source = settingsIcon;
+
+                    
+                }
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
